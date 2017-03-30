724f5a0096cbca47a22dee6dbf4ec0e6a043023c
haohuawu <haohuawu@qq.com>
2017-03-30 13:21:32 +0800

    fix 首页开启分页后置顶失效的问题

--- themes/hexo-theme-whaley/source/assets/api/js/main.js
+++ themes/hexo-theme-whaley/source/assets/api/js/main.js
@@ -1,1 +1,1 @@
-webpackJsonp([1,2],{0:function(n,e,t){t(2),t(5).attach(document.body)},1:function(n,e){},10:function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var c=t(1),o=(t.n(c),t(0));t.n(o)}},[10]);
+webpackJsonp([1],[function(n,e,t){t(2),t(6).attach(document.body)},function(n,e){},,function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var c=t(1),o=(t.n(c),t(0));t.n(o)}],[3]);
